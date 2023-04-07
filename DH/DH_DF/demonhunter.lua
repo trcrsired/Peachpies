dd27a1e2a00fbf45819f120475dc02185c9b7cac
trcrsired <uwgghhbcad@gmail.com>
2023-04-06 23:24:40 -0400

    initial attempt for vengence

--- DH/DH_DF/demonhunter.lua
+++ DH/DH_DF/demonhunter.lua
@@ -28,6 +28,7 @@ local monitored_spells =
 191427
 },
 {
+187827
 },
 }
 
@@ -55,16 +56,11 @@ local function cofunc(yd)
 		repeat
 		if yd == 0 then
 			specialization = GetSpecialization()
-			if specialization == 1 then
-				monitor_spells = monitored_spells[specialization]
-				grids_profile = Peachpies.GridsConfig(Peachpies.GetProfile(),grids_meta)
-				if grids_profile.Enable then
-					yd=coyield(2)
-				else
-					yd=coyield()
-				end
+			monitor_spells = monitored_spells[specialization]
+			grids_profile = Peachpies.GridsConfig(Peachpies.GetProfile(),grids_meta)
+			if grids_profile.Enable then
+				yd=coyield(2)
 			else
-				globalframe:Hide()
 				yd=coyield()
 			end
 			break
@@ -82,6 +78,7 @@ local function cofunc(yd)
 				local has_unbound_chaos = false
 				local has_momentum = false
 				local has_chaos_theory = false
+				local soul_fragments_count = 0
 				for i=1,100 do
 					local name, icon, count, debuffType, duration, expirationTime, source, isStealable, 
 					nameplateShowPersonal, spellId = UnitAura("PLAYER",i,"PLAYER|HELPFUL")
@@ -95,6 +92,8 @@ local function cofunc(yd)
 						has_momentum = true
 					elseif spellId == 390195 then
 						has_chaos_theory = true
+					elseif spellId == 203981 then
+						soul_fragments_count = count
 					end
 				end
 				local felrush_usable = is_spell_known(195072)
@@ -108,17 +107,35 @@ local function cofunc(yd)
 					local eye_beam_usable = is_spell_known_not_cooldown(198013)
 					local vengeful_retreat_usable = is_spell_known_not_cooldown(198793)
 					local essence_break_usable = is_spell_known_not_cooldown(258860)
-					local metamorphosis_usable = is_spell_known_not_cooldown(191427)
+					local metamorphosis_id = 191427
+					if specialization == 2 then
+						metamorphosis_id = 187827
+					end
+					local metamorphosis_usable = is_spell_known_not_cooldown(metamorphosis_id)
 					local annihilation_usable = is_spell_known_not_cooldown(201427)
 					local immolation_aura_usable = is_spell_known_not_cooldown(258920)
 					local felblade_usable = is_spell_known_not_cooldown(232893)
 					local sigilofflame_usable = is_spell_known_not_cooldown(204596)
 					local throwglaives_usable = is_spell_known_not_cooldown(185123)
+					local soul_carver_usable = is_spell_known_not_cooldown(207407)
 					local felrush_charges = 0
+					local fraility_stacks = 0
+
 					if felrush_usable then
 						felrush_charges = GetSpellCharges(195072)
 						if felrush_charges == nil then
 							felrush_charges = 0
+						end
+					end
+
+					local fiery_brand_usable = is_spell_known_not_cooldown(204021)
+
+
+					local fiery_brand_charges = 0
+					if fiery_brand_usable then
+						fiery_brand_usable = GetSpellCharges(204021)
+						if fiery_brand_usable == nil then
+							fiery_brand_usable = 0
 						end
 					end
 					local rounds = 5
@@ -126,11 +143,18 @@ local function cofunc(yd)
 						rounds = single_target_grids_count
 						start_grid = 1
 						end_grid = single_target_grids_count - 1
+
 					for i=1,rounds do
 						local single_spell = 203555
 						if the_hunt_usable then
 							single_spell = 370965
 							the_hunt_usable = false
+						elseif 4 <= soul_fragments_count and 0 < fiery_brand_charges then
+							single_spell = 204021
+							fiery_brand_charges = fiery_brand_charges - 1
+						elseif 6 <= fraility_stacks and soul_carver_usable then
+							single_spell = 207407
+							soul_carver_usable = false
 						elseif death_sweep_usable and fury >= 35 then
 							single_spell = 210152
 							fury = fury - 35
@@ -150,7 +174,7 @@ local function cofunc(yd)
 							single_spell = 258860
 							essence_break_usable = false
 						elseif metamorphosis_usable then
-							single_spell = 191427
+							single_spell = metamorphosis_id
 							metamorphosis_usable = false
 						elseif annihilation_usable and (has_essense_break or fury >= 40) then
 							single_spell = 201427
